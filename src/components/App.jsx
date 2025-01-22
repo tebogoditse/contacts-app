381f953b1d5f6150a9ca514d33ad955fc685ce6e
tebogoditse <tebogoditse2@gmail.com>
2025-01-22 16:11:48 +0200

    Refactoring the App.jsx code to look cleaner and shorter using es6

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,22 +2,11 @@ import React from "react";
 import contacts from "../contacts";
 import Card from "./Card";
 
-function createCard(card) {
-  return (
-    <Card
-      name = {card.name}
-      img = {card.imgURL}
-      phone = {card.phone}
-      email = {card.email} 
-    />
-  )
-}
-
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      {contacts.map(createCard)}
+      {contacts.map( card => <Card name = {card.name} img = {card.imgURL} phone = {card.phone} email = {card.email} />)}
     </div>
   );
 }
